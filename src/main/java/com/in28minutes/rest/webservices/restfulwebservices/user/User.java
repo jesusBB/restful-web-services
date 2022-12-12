b6902685418c97cc51898702663d785b300fab8c
Jesus Bustillo <jbustill@jaguarlandrover.com>
2022-12-12 13:56:36 +0000

    Using Spring Data JPA Repository to operate with User table

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,26 +4,33 @@
 
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
-
+@Entity(name ="USER_DETAILS")
 public class User {
-
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "Name should have at least 2 characters")
-    private final String name;
+    private String name;
 
     @Past(message = "Birth Date should be in the past")
-    private final LocalDate birthDate;
+    private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
         super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
     }
 
     public Integer getId() {
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Jaguar Land Rover Ltd 2022. All rights reserved
+ */
+
+insert into USER_DETAILS(id, birth_date, name)
+VALUES(10001, current_date(), 'Jesuli');
+insert into USER_DETAILS(id, birth_date, name)
+VALUES(10002, current_date(), 'Virginia');
+insert into USER_DETAILS(id, birth_date, name)
+VALUES(10003, current_date(), 'Miguel');
+insert into USER_DETAILS(id, birth_date, name)
+VALUES(10004, current_date(), 'Pepe');
