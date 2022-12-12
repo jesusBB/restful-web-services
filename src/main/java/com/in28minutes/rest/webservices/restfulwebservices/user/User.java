3f175fb9aa9a11aa5921b135ff6dcd5e4158eb65
Jesus Bustillo <jbustill@jaguarlandrover.com>
2022-12-12 14:45:25 +0000

    Creating Post Entity and its relationship with User entity (OneToMany and ManyToOne)

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,13 +4,17 @@
 
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
+
 @Entity(name ="USER_DETAILS")
 public class User {
     @Id
@@ -22,6 +26,10 @@ public class User {
 
     @Past(message = "Birth Date should be in the past")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         super();
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,3 +10,12 @@ insert into USER_DETAILS(id, birth_date, name)
 VALUES(10003, current_date(), 'Miguel');
 insert into USER_DETAILS(id, birth_date, name)
 VALUES(10004, current_date(), 'Pepe');
+
+insert into POST(id, description, user_id)
+VALUES(1, 'Message 1', 10001);
+insert into POST(id, description, user_id)
+VALUES(2, 'Message 2', 10001);
+insert into POST(id, description, user_id)
+VALUES(3, 'Message 1 user 2', 10002);
+insert into POST(id, description, user_id)
+VALUES(4, 'Message 1 user 3', 10003);
